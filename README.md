636d82d14fac4480b6b6157e83bf25be6bc801bc
Fredrik Bergqvist <fredrik@bergqvist.it>
2024-11-29 17:21:07 +0100

    First version of modal

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# Modal
+# Nidhugg Modal
+
+A basic web component for creating modals.
+
+## Basic usage
+
+1. Include the script in your HTML file:
+2. Add the `nidhugg-modal` element to your HTML file.
+3. Add content to the modal using the `header`, `content`, and `footer` slots.
+    
+    ```html
+<nidhugg-modal id="modal-1" open>
+  <h2 slot="header">Hello World</h2>
+  <p slot="content">This is a test of the Nidhugg Modal</p>
+  <button slot="footer" onclick="document.querySelector('#modal-1').close()">Close</button>
+</nidhugg-modal>
+```
+
+## Styling
+
+The modal can be styled using CSS custom variables. The following variables are available:
+
+```css
+:root{
+  --nidhugg-base-100:#2A303C;
+  --nidhugg-base-200:#242933;
+  --nidhugg-base-300:#20252E;
+  --nidhugg-base-content:#B2CCD6;
+  --nidhugg-neutral:#1C212B;
+  --nidhugg-neutral-content:#B2CCD6;
+  --nidhugg-rounded: 0.5rem;
+}
+```
+
+The modal will also add the `nidhugg-modal-open`-class to the body when it is open.
+
+```css
+/* Example: */
+ .nidhugg-modal-open{
+     overflow: hidden;
+     filter: blur(2px);
+     height: 100vh;
+     width: 100vw;
+ }
+```
---
+++ src/nidhugg-modal.js
@@ -0,0 +1,172 @@
+const modalStyles = `
+<style>
+  .nidhugg-modal__dialog{
+    display: none;
+    transition: all 0.3s ease-in-out;
+    opacity: 1;
+    transform: scale(1);
+    position: fixed;
+    z-index: 10;
+    width: 20rem;
+    padding: 0;
+    background-color: var(--nidhugg-base-100, #2A303C);
+    color: var(--nidhugg-base-content, #f2f2f2);
+    border-radius: var(--nidhugg-rounded, 0.5rem);
+    border: 2px solid var(--nidhugg-neutral, #1C212B);
+    box-shadow: 0 0 1rem rgba(0, 0, 0, 0.5);
+  }
+  .nidhugg-modal__dialog[open]::backdrop {
+    opacity: 0.6;
+    background-color: #111;
+  }
+  .nidhugg-modal__dialog[open] {
+    @starting-style{
+      opacity: 0;
+      transform: scale(0);
+    }
+    display: block;
+  }
+  .nidhugg-modal__dialog header {
+    background-color: var(--nidhugg-base-300, #20252E);
+    display: flex;
+    flex-direction: row-reverse;
+    justify-content: space-between;
+    align-items: flex-start;
+    padding: 1rem;
+  }
+  .nidhugg-modal__dialog button {
+    background-color: var(--nidhugg-base-100, #2A303C);
+    color: var(--nidhugg-base-content, #f2f2f2);
+    transition: background-color, color 0.3s ease-in-out;
+    border:none;
+    border-radius: 50%;
+    width: 2rem;
+    height: 2rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: -0.5rem;
+    margin-right: -0.5rem;
+    cursor: pointer;
+    &:hover {
+      color: var(--nidhugg-base-200, #242933);
+      background-color: var(--nidhugg-base-content, #f2f2f2);
+    }
+  }
+  
+  .nidhugg-modal__dialog main {
+    background-color: var(--nidhugg-base-100, #2A303C);
+    padding: 0 1rem 1rem;
+  }
+  .nidhugg-modal__dialog footer {
+    background-color: var(--nidhugg-base-100, #2A303C);
+    padding: 0 1rem 1rem;
+  }
+</style>
+`;
+
+class NidhuggModal extends HTMLElement {
+  static observedAttributes = ["open"];
+  constructor() {
+    super();
+    this.close = this.close.bind(this);
+  }
+
+  showModal() {
+    const dialog = this.shadowRoot.querySelector("dialog");
+    if(dialog) {
+      dialog.showModal();
+      document.body.classList.add("nidhugg-modal-open");
+    }
+  }
+
+  close()  {
+    console['log']('🐵: close', );
+    const dialog = this.shadowRoot.querySelector("dialog");
+    if(dialog) {
+      dialog.style.opacity = "0";
+      dialog.style.transform = "scale(0)";
+      setTimeout(() => {
+        dialog.close();
+        document.body.classList.remove("nidhugg-modal-open");
+        dialog.style.opacity = "";
+        dialog.style.transform = "";
+      }, 300);
+    }
+  }
+
+  populateElements() {
+    const headerTemplate = document.createElement('template');
+    const headingEl = `<header>
+      <button id="nidhugg-modal-close-btn" type="button" title="Close modal" autofocus>x</button>
+      <slot name="header"></slot>
+    </header>`;
+
+    const mainContentEl = `
+    <main class="nid-modal__main">
+      <slot name="content"></slot>
+    </main>`;
+
+    const footerEl = `
+    <footer class="nid-modal__footer">
+      <slot name="footer"></slot>
+    </footer>`;
+
+    headerTemplate.innerHTML = `${modalStyles}
+    <dialog class="nidhugg-modal__dialog">
+      ${headingEl}${mainContentEl}${footerEl}
+    </dialog>`;
+
+    this.shadowRoot.appendChild(headerTemplate.content.cloneNode(true));
+    if(this.hasAttribute("open")) {
+      this.showModal();
+    }
+  }
+
+
+  connectedCallback() {
+    this.attachShadow({mode: 'open'});
+    this.populateElements();
+
+    const dialogCloseBtn = this.shadowRoot.querySelector("#nidhugg-modal-close-btn");
+    dialogCloseBtn.addEventListener("click", (e) => {
+      this.close();
+    });
+    const dialogEl = this.shadowRoot.querySelector(".nidhugg-modal__dialog");
+    dialogEl.addEventListener('cancel', (event) => {
+      event.stopPropagation();
+      event.preventDefault();
+      this.close();
+    });
+    dialogEl.addEventListener('close', (event) => {
+      event.stopPropagation();
+      event.preventDefault();
+      this.close();
+    });
+  }
+
+  disconnectedCallback() {
+    console.log("Custom element removed from page.");
+  }
+
+  adoptedCallback() {
+    console.log("Custom element moved to new page.");
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    console.log(`Attribute ${name} has changed.`);
+    const dialog = this.shadowRoot && this.shadowRoot.querySelector("dialog");
+    if(!dialog) {
+      return;
+    }
+    if (name === "open") {
+      if (this.hasAttribute("open")) {
+        dialog.showModal();
+      } else {
+        dialog.close();
+      }
+    }
+  }
+}
+
+customElements.define("nidhugg-modal", NidhuggModal);
